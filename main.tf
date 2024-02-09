bede452b989969024cc93fbe7abaffcc2b3996e3
Murali Edupuganti <muraliedupuganti@Muralis-Air.attlocal.net>
2024-02-09 14:15:21 -0600

    Ec2 instance lanch

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+	region = "us-east-1"
+}
+
+
+resource "aws_instance" "vm" {
+	ami = "kld"
+	subnet_id = "kdfgjk"
+	instance_type = "t3.micor"
+	tags {
+		Name = "my-first-instance"
+	}
+}
+	
+
+
